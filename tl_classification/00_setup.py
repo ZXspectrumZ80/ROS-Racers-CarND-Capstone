5af00f042467ca2851a114aac3d945ac54edac05
kenny.lim <kenny.lim@daumkakao.com>
2018-04-06 12:49:18 +0900

    add other models

--- tl_classification/00_setup.py
+++ tl_classification/00_setup.py
@@ -12,3 +12,9 @@ if os.path.dirname(os.path.realpath(__file__)) != os.path.realpath(TL_CLASSIFICA
     print("* check TL_CLASSIFICATION_DIR in config.py")
     exit(-1)
 
+model_pretrained_dir = TL_CLASSIFICATION_DIR + "/model_pretrained"
+path, dirs, files = os.walk(model_pretrained_dir).next()
+if len(dirs) <= 0:
+    print("* check your model_pretrained directory : " + model_pretrained_dir)
+    print("* download and extract pretrained models into model_pretrained directory : https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md")
+    exit(-1)
