bbc2b9af2478943465cdaada4ac48fb1b4187492
kenny.lim <kenny.lim@daumkakao.com>
2018-04-06 09:52:48 +0900

    configs changed

--- tl_classification/05_export.py
+++ tl_classification/05_export.py
@@ -5,6 +5,10 @@ from config import *
 
 os.chdir(OD_RESEARCH_DIR)
 
+output_directory = OUTPUT_DIR + "/" + MODEL_NAME
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+
 cmd = "python object_detection/export_inference_graph.py"
 cmd += " --logtostderr"
 cmd += " --pipeline_config_path=" + CONFIG_PATH
--- tl_classification/config.py
+++ tl_classification/config.py
@@ -2,10 +2,10 @@ OD_RESEARCH_DIR = "/Users/illb/tensorflow/models/research"
 TL_CLASSIFICATION_DIR = "/Users/illb/carnd/ROS-Racers-CarND-Capstone/tl_classification"
 
 MODE = "sim" # sim | real
-CONFIG_FILENAME = "ssd_mobilenet_v1_coco.config"
+MODEL_NAME = "ssd_mobilenet_v1_coco"
 
 OUTPUT_DIR = TL_CLASSIFICATION_DIR + "/output_" + MODE
 
-CONFIG_INPUT_PATH = TL_CLASSIFICATION_DIR + "/model/" + CONFIG_FILENAME
-CONFIG_PATH = OUTPUT_DIR + "/" + CONFIG_FILENAME
+CONFIG_INPUT_PATH = TL_CLASSIFICATION_DIR + "/model_config/" + MODEL_NAME + ".config"
+CONFIG_PATH = OUTPUT_DIR + "/" + MODEL_NAME + ".config"
 EXPORT_CHECKPOINT_PATH = OUTPUT_DIR + "/model.ckpt-623"
